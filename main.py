315f8eae5a979d5a8b8eda85439c544de0d36e03
chenjie <skoody@gmail.com>
2024-05-16 23:16:40 -0700

    simple app

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import uvicorn
-from app.api import app 
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8088)
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
