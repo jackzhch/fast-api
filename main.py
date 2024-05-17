e7b49d1cae14fcc7408ad71c72d32d3da843f752
chenjie <skoody@gmail.com>
2024-05-17 00:26:33 -0700

    port set

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
     
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
-    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.api:app", port=port, reload=True)
