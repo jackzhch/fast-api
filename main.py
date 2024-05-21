887f12417bb87558c4f907598fd9e08898ceab9c
chenjie <skoody@gmail.com>
2024-05-20 23:53:26 -0700

    file location

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# from fastapi import FastAPI
-# from os import getenv
-# import uvicorn
+from dotenv import load_dotenv
+import os
+import uvicorn
 
-# app = FastAPI()
-    
-# if __name__ == "__main__":
-#     port = int(getenv("PORT", 8000))
-#     uvicorn.run("app.api:app", port=port, reload=True)
+load_dotenv()
+
+PORT = int(os.get('PORT', 8000))
+HOST = '0.0.0.0'
+
+if  __name__ == '__main__':
+    uvicorn.run('app.api:app', host=HOST, port=PORT, reload=True)
